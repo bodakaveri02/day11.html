cec88a1a780819ec94e0ca15abc43c647c22698e
bodakaveri02 <bodakaveri02@gmail.com>
2025-07-16 10:33:44 +0530

    created string.js
    added properties of string in js.

---
+++ string.js
@@ -0,0 +1,14 @@
+a="hello"
+b=a.slice(0,2);
+console.log(b);
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+a.trim();
+console.log(a);
+b=a.trim();
+c=a.length;
+console.log(c)
+d=a.replace("hello","hi good morning");
+console.log(d)
+console.log(d.repeat(3));
+
