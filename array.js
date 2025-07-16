33df1498a7068709f187cfcea4d77f8854218359
bodakaveri02 <bodakaveri02@gmail.com>
2025-07-16 10:25:59 +0530

    created array.js
    added array properties.

---
+++ array.js
@@ -0,0 +1,8 @@
+let num1=["apple","mango","guava"];
+let num2=["banana",1]
+console.log(num1.length);
+console.log(num1[0][4]);
+console.log(num1[1][4]);
+console.log(num2);
+console.log(num2.length);
+console.log(num2[1]="orange");
