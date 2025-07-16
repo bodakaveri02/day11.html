b296f511b69289d70ddf15f360f3b64282f724c7
bodakaveri02 <bodakaveri02@gmail.com>
2025-07-16 10:28:33 +0530

    created conditional.js
    added conditional properties and let var prompts in .js.

---
+++ conditional.js
@@ -0,0 +1,28 @@
+/*a=10;
+if(a==10){
+    if(a<15){
+         console.log("a is lessthan  15");
+        if(a<12)
+            console.log("a is lessthan 12");
+         else
+            console.log("a is greater  15");
+    }
+}
+/*empty,null,undefined,0,NaN are false
+if(10){
+    console.log("true");
+}
+else{
+    console.log("false");
+}*/
+ /*console.error("Error: Something went wrong!");
+console.warn("Warning: Your battery is low!");*/
+ let f=prompt("enter your name");
+let g=prompt("enter your last name");
+ console.log("welcome",f,g,"!");
+  var flag=confirm("are you sure");
+ if(flag==true)
+    console.log("clicked on ok");
+ else
+    console.log("clicked on cancel");
+ 
